3b098a41a7a91aa0c19d9f768c2227525411c484
Meepster27 <sharpieisme27@gmail.com>
2026-03-29 17:45:43 -0500

    slighty wrong now

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
-import { View } from "react-native";
+import { StatusBar, View } from "react-native";
 import Box from "./box";
+import Column from "./column";
+import Row from "./row";
 import styles from "./styles";
+
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Box>#1</Box>
-      <Box>#2</Box>
-    </View>
-  );
+  return (
+    <View style={styles.container}>
+      <StatusBar hidden={false} />
+      <Row>
+        <Column>
+          <Box>#1</Box>
+          <Box>#2</Box>
+        </Column>
+        <Column>
+          <Box>#3</Box>
+          <Box>#4</Box>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Box>#5</Box>
+          <Box>#6</Box>
+        </Column>
+        <Column>
+          <Box>#7</Box>
+          <Box>#8</Box>
+        </Column>
+      </Row>
+      <Row>
+        <Column>
+          <Box>#9</Box>
+          <Box>#10</Box>
+        </Column>
+        <Column>
+          <Box>#11</Box>
+          <Box>#12</Box>
+        </Column>
+      </Row>
+    </View>
+  );
 }
--- styles.js
+++ styles.js
@@ -1,27 +1,32 @@
 import { Platform, StatusBar, StyleSheet } from "react-native";
+
 export default StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    backgroundColor: "ghostwhite",
-    justifyContent: "space-around",
-    ...Platform.select({
-      ios: { paddingTop: 20 },
-      android: { paddingTop: StatusBar.currentHeight },
-    }),
-  },
-  box: {
-    height: 100,
-    justifyContent: "center",
-    alignSelf: "stretch",
-    alignItems: "center",
-    backgroundColor: "lightgray",
-    borderWidth: 1,
-    borderStyle: "dashed",
-    borderColor: "darkslategray",
-  },
-  boxText: {
-    color: "darkslategray",
-    fontWeight: "bold",
-  },
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    backgroundColor: "ghostwhite",
+    alignItems: "center",
+    ...Platform.select({
+      ios: { paddingTop: 40 },
+      android: { paddingTop: StatusBar.currentHeight },
+    }),
+  },
+
+  box: {
+    height: 100,
+    width: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "lightgray",
+    borderWidth: 1,
+    borderStyle: "dashed",
+    borderColor: "darkslategray",
+    margin: 10,
+  },
+
+  boxText: {
+    color: "darkslategray",
+    fontWeight: "bold",
+  },
 });
